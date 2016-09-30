b85b0761f4840eb74392233b2e0ef8d7be874ae1
shengxihu <1679112936@qq.com>
2016-09-30 15:56:36 +0800

    _

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,30 @@
 module.exports = function (app) {
+    var hbs = require('hbs');
 	var request = require('superagent');
 	var jsonp = require('superagent-jsonp');
     // app.use('/', require('./page1'));
     // app.use('/page2', require('./page2'));
+    
+    // 指定模板文件的后缀名为html
+    app.set('view engine', 'html');
+
+    // 运行hbs模块
+    app.engine('html', hbs.__express);
+
     app.get('/', function(req, res) {
     	res.render('page1');
 	});
     app.get('/:id', function(req, res) {
-    	res.render('page2');
+    	var arg = req.params.id;
+        var url = 'https://api.douban.com/v2/book/' + arg;
+        request
+            .get(url)
+            .use(jsonp)
+            .end(function(err, resp){
+                if(err){
+                    console.log('Something error!')
+                }
+                res.render('../views/page2/index',{book: resp.body.title});
+            })
 	});
 };
